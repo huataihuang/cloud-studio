21608e09112740f42bf3ab1e0f1bebf648ba1e61
huataihuang <huataihuang@gmail.com>
2025-05-31 17:12:31 +0800

    golang lsp

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -11,7 +11,7 @@ require("mason").setup({
 
 require("mason-lspconfig").setup({
 	-- A list of servers to automatically install if they're not already installed
-	ensure_installed = { "pylsp", "lua_ls", "bashls", "clangd", "ruby_lsp" },
+	ensure_installed = { "pylsp", "lua_ls", "bashls", "clangd", "ruby_lsp", "gopls" },
 })
 
 -- Set different settings for different languages' LSP
